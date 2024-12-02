c08f9f4657a3b0e80c663359a2adc1b8e5225dd3
fhrenne inso <inso_f@f112.lan>
2024-12-02 16:09:55 +0800

    Compilationnnnnnnn

--- MiniProjectB/src/main.jsx
+++ MiniProjectB/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx'; // Ensure this points to your App component
-import './index.css'; // Include your global styles
+import App from './App.jsx'; 
+import './index.css'; 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
